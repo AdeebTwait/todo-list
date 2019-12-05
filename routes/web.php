7e3e58559f06a7b5fbe33f74b2ec8bc2602100dc
Adeeb Twait <adeebtwait@gmail.com>
2019-12-05 15:50:25 +0200

    Fix bug

--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,11 @@ Route::group(['middleware' => ['auth']], function () {
 	Route::get('/', function () {
     	return redirect()->route('tasks.show', auth()->user()->id);
 	});
+	Route::get('/home', function () {
+    	return redirect()->route('tasks.show', auth()->user()->id);
+	});
 
 	Task::routes();
-
-	Route::get('/home', 'HomeController@index')->name('home');
 
 	Route::get('/users/{user}', 'UserController@edit')->name('user.edit');
 	Route::put('/users/{user}/update', 'UserController@update')->name('user.update');
